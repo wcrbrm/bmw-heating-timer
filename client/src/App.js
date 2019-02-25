ecbbecc7129a292e2d467b9bb7559f6238f8d0f0
wcrbrm <viclopata@gmail.com>
2019-02-25 12:20:00 +0200

    initial project

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import logo from './bmw.svg';
+import './App.css';
+import Switch from "react-switch";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Enabled: <Switch onChange={() => {} } checked={true} />
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
